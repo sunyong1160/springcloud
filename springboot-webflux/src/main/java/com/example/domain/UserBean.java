c978846b407af7bb6dc8747b22e55de484991c1c
sunyong <sunyong@yoozoo.com>
2019-08-24 12:28:30 +0800

    自定义解析器

--- springboot-webflux/src/main/java/com/example/domain/UserBean.java
+++ springboot-webflux/src/main/java/com/example/domain/UserBean.java
@@ -1,10 +1,12 @@
 package com.example.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
 public class UserBean implements Serializable {
 
     private Long id;
--- springboot-webflux/src/main/java/com/example/resolver/MyCurrentUserResolver.java
+++ springboot-webflux/src/main/java/com/example/resolver/MyCurrentUserResolver.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
 import java.util.Objects;
 
 
@@ -32,8 +33,15 @@ public class MyCurrentUserResolver implements HandlerMethodArgumentResolver {
     public Object resolveArgument(MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer, NativeWebRequest nativeWebRequest, WebDataBinderFactory webDataBinderFactory) throws Exception {
         // 从session中获取用户信息
         HttpServletRequest request = nativeWebRequest.getNativeRequest(HttpServletRequest.class);
+        // 后续可以做加解密处理
+        String header = request.getHeader("enen");
+        if("1".equals(header)){
+            return new UserBean(2l, "lisi");
+        }
         String body = new String(IOUtils.toByteArray(Objects.requireNonNull(request).getInputStream()));
         UserBean userBean = JSONObject.parseObject(body, UserBean.class);
+        //处理业务逻辑操作
+        userBean.setUsername(new String(Base64.getEncoder().encode(userBean.getUsername().getBytes())));
         return userBean;
     }
 }
